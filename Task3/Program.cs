d2a31abddf04689d4b0965e45637d398c1d9aafe
IlyaSolovyov <242695186+ilya-salauyou@users.noreply.github.com>
2025-11-17 16:10:29 +0300

    Task 3: Работа над ошибками v2

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,19 +7,27 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            var sales = new List<Sale>()
+            try
             {
-                new Sale(154335m, "Sanya"),
-                new Sale(122727m, "Sanya"),
-                new Sale(250001m, "Sanya"),
-                new Sale(20193m, "Kiril"),
-                new Sale(30660m, "Kiril"),
-                new Sale(60740m, "Kiril"),
-                new Sale(900m, "Tolik")
-            };
+                var sales = new List<Sale>()
+                {
+                    new Sale(154335m, "Sanya"),
+                    new Sale(122727m, "Sanya"),
+                    new Sale(250001m, "Sanya"),
+                    new Sale(20193m, "Kiril"),
+                    new Sale(30660m, "Kiril"),
+                    new Sale(60740m, "Kiril"),
+                    new Sale(900m, "Tolik")
+                };
 
-            var bounuses = BonusCalculator.CalculateBonus(sales);
-            BonusCalculator.PrintBonuses(bounuses);
+                var bounuses = BonusCalculator.CalculateBonus(sales);
+                BonusCalculator.PrintBonuses(bounuses);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
--- Task3/Sale.cs
+++ Task3/Sale.cs
@@ -1,4 +1,6 @@
-﻿namespace Task3
+﻿using System;
+
+namespace Task3
 {
     public class Sale
     {
@@ -7,6 +9,11 @@
 
         public Sale(decimal sum, string manager)
         {
+            if (sum <= 0) 
+                throw new Exception("Сумма продажи не может быть отрицательной");
+            if (string.IsNullOrWhiteSpace(manager)) 
+                throw new Exception("Имя менеджера должно быит не пустое");
+
             Sum = sum;
             Manager = manager;
         }
