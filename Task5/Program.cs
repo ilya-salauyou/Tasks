801d729684299a9e26fff10972279412e238c56f
IlyaSolovyov <242695186+ilya-salauyou@users.noreply.github.com>
2025-11-20 15:34:47 +0300

    Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            try
+            {
+                string data = await AsyncMethod();
+                Console.WriteLine("ПЛАГИН: Основная транзакция завершена");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        async static Task<string> AsyncMethod()
+        {
+            Thread.Sleep(4000);
+            return "anyData";
+        }
+    }
+}
